2695056e10c4a7d697aea165b4fe2c35f5f0833a
kmather73 <kmather73@gmail.com>
2017-05-22 13:13:28 -0500

    Done 805A

---
+++ C++/805A_Fake_NP.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+	int l, r;
+	 cin >> l >> r;
+
+	 if(l == r) cout << l << endl;
+	 else cout << 2 << endl;
+
+	return 0;
+}
